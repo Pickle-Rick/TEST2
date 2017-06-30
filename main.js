0b3a0d7050d883f276c197132fa26740828a9d42
Pickle-Rick <c_sganga@aol.com>
2017-06-29 22:14:19 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+var myName = "Chris's Apples";
+
+var black = [0, 0, 0];
+var black = [0, 0, 0];
+var black = [0, 0, 0];
+var blue = [0,0, 0];
+var purple = [0, 0, 0];
+var letterColors = [black, black, black, black, black];
+
+drawName(myName, letterColors);
+
+if(10 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'circle';
+}
+
+bounceBubbles();
