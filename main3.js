a206eba2ba3e52b278f94c50a8ac496043a2c561
Pickle-Rick <c_sganga@aol.com>
2017-07-15 16:43:33 -0400

    Update main3.js

--- main3.js
+++ main3.js
@@ -1,7 +1,10 @@
-function main() {
-  $('b.burger-nav').on('click', function() {
-    $('.butt').toggleClass('open');
-    $('.butt').slideToggle(700);
+$(document).ready(function() {
+  var par = $('.butt');
+  $(par).hide();
+  
+  $('b.burger-nav').click(function(e) {
+  	  //$(par).toggleClass('open');
+      $(par).slideToggle(700);
+      e.preventDefault();
   });
-}
-$(document).ready(main);
+});
